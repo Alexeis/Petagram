0fa93f3d6683d2c9673e10759c35374e18b925e6
Alexeis <alexey.figueroa@gmail.com>
2019-02-03 23:03:29 -0600

    Salva

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.petagram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar miActionBar = findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
     }
 }
